39a18c64519997afcff2a48e3da57bba169af1be
malapakavivek <89408748+malapakavivek@users.noreply.github.com>
2023-05-08 15:41:24 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("Sally Rooney");
+  const [author, setAuthor] = useState("");
   const [isPending, setIsPending]= useState(false)
   const history = useHistory();
   
@@ -46,13 +46,12 @@ const Create = () => {
 
 
         <label>Blog Author </label>
-        <select 
-            value={author} 
-            onChange={(e) => setAuthor(e.target.value)}>
-          <option value="J. K. Rowling">J. K. Rowling</option>
-          <option value="J. R. R. Tolkien">J. R. R. Tolkien</option>
-          <option value="Sally Rooney">Sally Rooney</option>
-        </select>
+        <input 
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
 
         {!isPending && <button>Add Blog</button>}
         { isPending && <button>Adding Blog</button>}
